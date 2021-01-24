e3714080484b599a0f62db4ec45a961a5d4996f9
Pedro <pedro.munozlopez@icloud.com>
2021-01-24 02:09:53 -0300

    casi OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,32 +15,32 @@ $(document).ready(function () {
         $('[data-toggle="popover"]').popover()
       });
 
+      $("#uno").click(function () {
+          $("iframe").attr("src", "https://pedromunozlopez86.github.io/meet-coffe/");
+      });
+      
+      $("#dos").click(function () {
+          $("iframe").attr("src", "https://pedromunozlopez86.github.io/iguana/");
+        });
+      
+        $("#tres").click(function () {
+          $("iframe").attr("src", "https://pedromunozlopez86.github.io/Suricata/");
+        });
+      
+        $("#cuatro").click(function () {
+          $("iframe").attr("src", "https://pedromunozlopez86.github.io/Cuppon/");
+        });
+      
+        $("#cinco").click(function () {
+          $("iframe").attr("src", "https://pedromunozlopez86.github.io/Ricomida/");
+        });
+      
+        $("#seis").click(function () {
+          $("iframe").attr("src", "https://www.atp.com");
+        });
     
 })
 
-    $("#uno").click(function () {
-        $("iframe").attr("src", "https://pedromunozlopez86.github.io/meet-coffe/");
-    });
-
-    $("#dos").click(function () {
-        $("iframe").attr("src", "https://pedromunozlopez86.github.io/iguana/");
-      });
-
-      $("#tres").click(function () {
-        $("iframe").attr("src", "https://pedromunozlopez86.github.io/Suricata/");
-      });
-
-      $("#cuatro").click(function () {
-        $("iframe").attr("src", "https://pedromunozlopez86.github.io/Cuppon/");
-      });
-
-      $("#cinco").click(function () {
-        $("iframe").attr("src", "https://pedromunozlopez86.github.io/Ricomida/");
-      });
-
-      $("#seis").click(function () {
-        $("iframe").attr("src", "https://www.atp.com");
-      });
 
 
 
